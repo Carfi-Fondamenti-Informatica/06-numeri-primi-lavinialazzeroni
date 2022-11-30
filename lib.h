b9c001a62c00c23e6900b1c8f4e544ad3f3370fd
lavinialazzeroni <118049473+lavinialazzeroni@users.noreply.github.com>
2022-11-30 16:13:52 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,19 @@
-
+#include<iostream>
+using namespace std;
+int numeroprimo (int n) {
+    int i=1, x=0;
+    char w;
+if (n>1) {
+do {
+    i++;
+    x = n%i;
+}
+while (x!=0);
+if (n == i) {
+    w = true;
+}
+else {
+    w = false;
+}
+}
+}
