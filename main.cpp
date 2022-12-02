e0de9fe8d5c9ffcdb13d398dc79ee7f39435781d
lavinialazzeroni <118049473+lavinialazzeroni@users.noreply.github.com>
2022-12-02 18:38:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
-
-int main(){
-  return 0;
+#include<iostream>
+#include "lib.h"
+int main() {
+    int h;
+    std::cin >> h;
+    if (numeroprimo(h) == 0) {
+        std::cout << "numero primo" << std::endl;
+    }
+    else {
+        std::cout << "numero non primo" << std::endl;
+    }
+    return 0;
 }
